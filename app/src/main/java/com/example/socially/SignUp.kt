f688828ffb12964f5c997c2c032932ca76089ec4
Humayun <humayunbilal04@gmail.com>
2025-09-02 21:47:49 +0500

    did 4 intro screens and navigation, back buton is left

--- app/src/main/java/com/example/socially/SignUp.kt
+++ app/src/main/java/com/example/socially/SignUp.kt
@@ -1,20 +1,23 @@
 package com.example.socially
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SignUp : AppCompatActivity() {
+class signUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val signUpButton = findViewById<Button>(R.id.createAcc)
+
+        signUpButton.setOnClickListener {
+            val intent = Intent(this, switchAccounts::class.java)
+            startActivity(intent)
         }
     }
 }
